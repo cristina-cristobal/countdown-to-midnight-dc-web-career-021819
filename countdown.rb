c7738e68b32c4ade936e653e854c29e6e0f48e5b
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-05 19:28:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(seconds)
 end
    returns "HAPPY NEW YEAR!"
 end
+
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
